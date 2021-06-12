7c3a201bc647d4c78be95a5edd294a968443bf99
koyo <nexus9071@gmail.com>
2021-06-12 21:07:43 +0900

    Initialize tailwind

---
+++ components/Layout.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+
+type Props = {
+  children?: ReactNode
+  title?: string
+}
+
+const Layout = ({ children, title = 'This is the default title' }: Props) => (
+  <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>{' '}
+        |{' '}
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
+    </header>
+    <main className="flex flex-col items-center justify-center w-full flex-1 text-center">
+    {children}
+    </main>
+    <footer className="flex items-center justify-center w-full h-24 border-t">
+      Footer
+    </footer>
+  </div>
+)
+
+export default Layout
