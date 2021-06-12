77cc1a59bebdfa47946cde90b11027833e20a4ab
koyo-miyamura <nexus9071@gmail.com>
2021-06-13 00:40:14 +0900

    OGPカードコンポーネントの実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
+import ProfileCard from '../components/ProfileCard'
+import useSWR from 'swr'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+type OgpData = {
+  title: string,
+  image: string,
+  description: string,
+}
+
+const IndexPage = () => {
+  const url = "https://github.com/koyo-miyamura"
+  const {data} = useSWR<OgpData>(`/api/ogp?url=${url}`)
+
+  console.log(data)
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <ProfileCard title={data?.title} imageUrl={data?.image} content={data?.description} />
+    </Layout>
+  )
+}
 
 export default IndexPage
