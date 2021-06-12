07ba2acf99b649227b9578c68a89792e1898e255
koyo-miyamura <nexus9071@gmail.com>
2021-06-13 01:22:44 +0900

    lgつけとく

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 const ProfileCard = ({ title = "", content= "", imageUrl = "" }: Props) => (
-  <div className="lg:m-4 w-1/3 shadow-md hover:shadow-lg hover:bg-gray-100 rounded-lg bg-white my-12 mx-8">
+  <div className="lg:m-4 lg:w-1/3 shadow-md hover:shadow-lg hover:bg-gray-100 rounded-lg bg-white my-12 mx-8">
     <img src={imageUrl} alt=""className="overflow-hidden" />
     <div className="p-4">
       <h3 className="font-medium text-gray-600 text-lg my-2 uppercase">{title}</h3>
